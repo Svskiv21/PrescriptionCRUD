5848001781eb23a62c1242d0b31ccb98718a7f21
Svskiv21 <nataliakrukar@gmail.com>
2022-01-26 20:53:19 +0100

    Revert "fixed PatientController and PrescriptionController with changed endpoints"
    This reverts commit 0dba35dd6ba5554c219af8ce89a97adc01d2e41e.

--- src/main/java/com/example/PrescriptionCRUD/controllers/PatientController.java
+++ src/main/java/com/example/PrescriptionCRUD/controllers/PatientController.java
@@ -4,7 +4,6 @@ import com.example.PrescriptionCRUD.dtos.PatientCreateDTO;
 import com.example.PrescriptionCRUD.dtos.PatientShowDTO;
 import com.example.PrescriptionCRUD.services.PatientService;
 import lombok.AllArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,30 +16,26 @@ public class PatientController {
     private final PatientService patientService;
 
     @GetMapping(path = "/all-patients")
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     public List<PatientShowDTO> getPatients(){
         return patientService.getAllPatients();
     }
 
-    @GetMapping("/{id}")
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    @GetMapping("/all-patients/{id}")
     public PatientShowDTO getSinglePatient(@PathVariable Long id){
         return patientService.getPatient(id);
     }
 
-//    @PostMapping(path = "/new-patient")
-//    public void addPatient(@RequestBody PatientCreateDTO patientCreateDTO){
-//        patientService.addPatient(patientCreateDTO);
-//    }
+    @PostMapping(path = "/new-patient")
+    public void addPatient(@RequestBody PatientCreateDTO patientCreateDTO){
+        patientService.addPatient(patientCreateDTO);
+    }
 
-    @DeleteMapping(path = "/{id}")
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    @DeleteMapping(path = "all-patients/{id}")
     public void deletePatient(@PathVariable("id") Long patientId){
         patientService.deletePatient(patientId);
     }
 
     @PutMapping(path = "/{patientId}")
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     public void updatePatient(@PathVariable Long patientId, @RequestParam String name, @RequestParam String lastName){
         patientService.updatePatient(patientId, name, lastName);
     }
--- src/main/java/com/example/PrescriptionCRUD/controllers/PrescriptionController.java
+++ src/main/java/com/example/PrescriptionCRUD/controllers/PrescriptionController.java
@@ -2,9 +2,9 @@ package com.example.PrescriptionCRUD.controllers;
 
 import com.example.PrescriptionCRUD.dtos.PrescriptionCreateDTO;
 import com.example.PrescriptionCRUD.dtos.PrescriptionShowDTO;
+import com.example.PrescriptionCRUD.entities.Prescription;
 import com.example.PrescriptionCRUD.services.PrescriptionService;
 import lombok.Data;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,19 +17,16 @@ public class PrescriptionController {
     private final PrescriptionService prescriptionService;
 
     @GetMapping
-    @PreAuthorize("hasAnyRole('ROLE_DOCTOR', 'ROLE_PATIENT', 'ROLE_ADMIN')")
     public List<PrescriptionShowDTO> getPrescriptions(){
         return prescriptionService.getPrescriptions();
     }
 
     @PostMapping
-    @PreAuthorize("hasAnyRole('ROLE_DOCTOR', 'ROLE_ADMIN')")
     public void addNewPrescription(@RequestBody PrescriptionCreateDTO prescription){
         prescriptionService.addNewPrescription(prescription);
     }
 
     @DeleteMapping(path = {"prescriptionId"})
-    @PreAuthorize("hasAnyRole('ROLE_DOCTOR', 'ROLE_ADMIN')")
     public void deletePrescription(@PathVariable("prescriptionId") Long prescriptionId){
         prescriptionService.deletePrescription(prescriptionId);
     }
