1d403fdcec007ed4f1d9ffd9f448aabb0de3ecc6
Svskiv21 <nataliakrukar@gmail.com>
2022-01-26 20:53:58 +0100

    Revert "added UserService"
    This reverts commit 425a48e75f168eb0f3e4ada3bc661f26ec445c07.

--- src/main/java/com/example/PrescriptionCRUD/mappers/UserMapper.java
+++ src/main/java/com/example/PrescriptionCRUD/mappers/UserMapper.java
@@ -3,7 +3,6 @@ package com.example.PrescriptionCRUD.mappers;
 import com.example.PrescriptionCRUD.dtos.DoctorCreateDTO;
 import com.example.PrescriptionCRUD.dtos.PatientCreateDTO;
 import com.example.PrescriptionCRUD.dtos.UserDTO;
-import com.example.PrescriptionCRUD.entities.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
@@ -13,9 +12,7 @@ public interface UserMapper {
 
     static final UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
-    User userDTOtoUser(UserDTO userDTO);
+    DoctorCreateDTO UserDTOtoDoctorDTO(UserDTO userDTO);
 
-    DoctorCreateDTO userDTOtoDoctorDTO(UserDTO userDTO);
-
-    PatientCreateDTO userDTOtoPatientDTO(UserDTO userDTO);
+    PatientCreateDTO UserDTOtoPatientDTO(UserDTO userDTO);
 }
--- src/main/java/com/example/PrescriptionCRUD/services/UserService.java
+++ src/main/java/com/example/PrescriptionCRUD/services/UserService.java
@@ -1,15 +1,10 @@
 package com.example.PrescriptionCRUD.services;
 
 import com.example.PrescriptionCRUD.dtos.UserDTO;
-import com.example.PrescriptionCRUD.entities.User;
 import com.example.PrescriptionCRUD.entities.UserType;
-import com.example.PrescriptionCRUD.mappers.UserMapper;
 import com.example.PrescriptionCRUD.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
-import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -19,35 +14,11 @@ public class UserService {
     private final PatientService patientService;
     private final DoctorService doctorService;
 
-    @Transactional
     public void createUser(UserDTO userDTO){
-        Optional<User> optionalUser = userRepository.findByPesel(userDTO.getPesel());
-        if (optionalUser.isEmpty()){
-            if (userDTO.getUserType() == UserType.DOCTOR){
-                doctorService.addNewDoctor(UserMapper.INSTANCE.userDTOtoDoctorDTO(userDTO));
-            } else {
-                patientService.addPatient(UserMapper.INSTANCE.userDTOtoPatientDTO(userDTO));
-            }
-            User user = UserMapper.INSTANCE.userDTOtoUser(userDTO);
-            userRepository.save(user);
+        if (userDTO.getUserType() == UserType.DOCTOR){
+
         } else {
-            throw new IllegalStateException("User already exists in database!");
+
         }
-    }
-
-    // tylko dla admina
-    public Long getUserId(String userPesel){
-        User user = userRepository.findByPesel(userPesel)
-                .orElseThrow(() -> new IllegalStateException("User does not exist in database."));
-        return user.getId();
-    }
-
-    // tylko dla admina
-    @Transactional
-    public void deleteUser(Long userId){
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalStateException("User does not exist in database."));
-
-        userRepository.deleteById(userId);
     }
 }
