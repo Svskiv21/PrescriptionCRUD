63316cd3fc67d72978a977614b0105002b3fd874
Svskiv21 <nataliakrukar@gmail.com>
2022-01-26 20:54:08 +0100

    Revert "added UserRepository and UserService"
    This reverts commit b9b0a5e35a567ab50a1c6d41d6b5a6aa4b8458cd.

--- src/main/java/com/example/PrescriptionCRUD/repositories/PatientRepository.java
+++ src/main/java/com/example/PrescriptionCRUD/repositories/PatientRepository.java
@@ -9,5 +9,6 @@ import java.util.Optional;
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
+
     Optional<Patient> findByPesel(String pesel);
 }
--- src/main/java/com/example/PrescriptionCRUD/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.PrescriptionCRUD.repositories;
-
-import com.example.PrescriptionCRUD.entities.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-}
